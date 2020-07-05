f5267513b921f87e9f42220c24ddbc4bcf2ab204
James Atkinson <james.atkinson@investopedia.com>
2020-07-05 17:17:17 -0600

    PM2 setup

--- start-pi.sh
+++ start-pi.sh
@@ -1,6 +1,7 @@
 if [ -z "$DISPLAY" ]; then #If not set DISPLAY is SSH remote or tty
 	export DISPLAY=:0 # Set by default display
 fi
+cd ~/infoboard
 yarn start:server > /dev/null &
 yarn start:client > /dev/null &
 sleep 10
